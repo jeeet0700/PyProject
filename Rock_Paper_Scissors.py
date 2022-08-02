cc82f89afc429c4f3fb403c8a14ae20196beb9cd
jitendra <jitendra_mahato@gmail.com>
2022-08-02 20:03:41 +0530

    s

--- Rock_Paper_Scissors.py
+++ Rock_Paper_Scissors.py
@@ -1,1 +1,23 @@
+import random
 
+
+def play():
+    user = input("'r' for rock,'p' for paper, 's' for scissors")
+    computer = random.choice(['r','p','s'])
+
+    if user == computer:
+        return 'It is tie'
+
+    if is_win(user,computer):
+        return 'You Won'
+
+    return 'You lost'
+
+def is_win(player,opponent):
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p')\
+        or (player =='p' and opponent == 'r'):
+        return True
+
+
+
+print(play())
